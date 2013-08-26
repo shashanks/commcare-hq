bf3956342d5aad40d3f583532f38c69977e29a9b
Sravan Reddy <sravfeyn@gmail.com>
2013-08-26 11:28:22 +0530

    refactored into single view, sql_reports in progress

--- custom/apps/gsid/_design/views/patient_summary/map.js
+++ custom/apps/gsid/_design/views/patient_summary/map.js
@@ -1,24 +1,23 @@
 function (doc) {
-    //!code util/emit_array.js 
     if (doc.doc_type === 'XFormInstance' && (doc.domain === 'gsid')) {
         var form = doc.form;
 
         if (form["@name"] !== 'Malaria Test' && form["@name"] !== 'HIV Test') {
             return;
         }
- 
-        var data = {
-			"age" : parseInt(form.age, 10),
-			"diagnosis" : form.diagnosis
-		};
 
-        emit_array([
-			doc.domain,
-			form["@name"],
-			form.test_version,
-			form.province,
-			form.district,
-			form.clinic,
-			form.sex], [doc.received_on], data);
+        emit(
+            [doc.domain,
+            form["@name"],
+            form.test_version,
+            form.province,
+            form.district,
+            form.clinic,
+            form.sex,
+            doc.received_on,
+            form.diagnosis,
+            parseInt(form.lot_number, 10)],
+            parseInt(form.age, 10)
+        );
     }
 }
--- custom/apps/gsid/ctable_mappings.py
+++ custom/apps/gsid/ctable_mappings.py
@@ -18,12 +18,9 @@ class PatientSummaryMapping(CtableMappingFixture):
             ColumnDef(name="district", data_type="string", value_source="key", value_index=4),
             ColumnDef(name="clinic", data_type="string", value_source="key", value_index=5),
             ColumnDef(name="gender", data_type="string", value_source="key", value_index=6),
-            ColumnDef(name="date", data_type="date", value_source="key", value_index=1),
-            ColumnDef(name="age", data_type="integer", value_source="value", value_attribute="sum", 
-                      match_keys=[KeyMatcher(index=7, value="age")]),
-            ColumnDef(name="diagnosis", data_type="string", value_source="value", value_attribute="sum", 
-                      match_keys=[KeyMatcher(index=7, value="diagnosis")])         
-
+            ColumnDef(name="date", data_type="date", value_source="key", value_index=7),
+            ColumnDef(name="diagnosis", data_type="string", value_source="key", value_index=8),
+            ColumnDef(name="age", data_type="integer", value_source="value", value_attribute="sum"),       
         ]
 
         return columns
@@ -31,7 +28,7 @@ class PatientSummaryMapping(CtableMappingFixture):
 class ResultsByDayMapping(CtableMappingFixture):
     name = "results_by_day"
     domains = ['gsid']
-    couch_view = 'gsid/results_by_day'
+    couch_view = 'gsid/patient_summary'
     schedule_active = True
 
     @property
@@ -52,7 +49,7 @@ class ResultsByDayMapping(CtableMappingFixture):
 class TestLotsMapping(CtableMappingFixture):
     name = "test_lots"
     domains = ['gsid']
-    couch_view = 'gsid/test_lots'
+    couch_view = 'gsid/patient_summary'
     schedule_active = True
 
     @property
@@ -64,8 +61,8 @@ class TestLotsMapping(CtableMappingFixture):
             ColumnDef(name="province", data_type="string", value_source="key", value_index=3),
             ColumnDef(name="district", data_type="string", value_source="key", value_index=4),
             ColumnDef(name="clinic", data_type="string", value_source="key", value_index=5),
-            ColumnDef(name="date", data_type="string", value_source="key", value_index=6),
-            ColumnDef(name="lot_number", data_type="integer", value_source="key", value_index=7),
+            ColumnDef(name="date", data_type="string", value_source="key", value_index=7),
+            ColumnDef(name="lot_number", data_type="integer", value_source="key", value_index=9),
         ]
 
         return columns
@@ -73,7 +70,7 @@ class TestLotsMapping(CtableMappingFixture):
 class ResultsByAgeMapping(CtableMappingFixture):
     name = "results_by_age"
     domains = ['gsid']
-    couch_view = 'gsid/results_by_age'
+    couch_view = 'gsid/patient_summary'
     schedule_active = True
 
     @property
@@ -87,16 +84,7 @@ class ResultsByAgeMapping(CtableMappingFixture):
             ColumnDef(name="clinic", data_type="string", value_source="key", value_index=5),
             ColumnDef(name="gender", data_type="string", value_source="key", value_index=6),
             ColumnDef(name="date", data_type="date", value_source="key", value_index=7),
-            ColumnDef(name="age", data_type="integer", value_source="key", value_index=8),
+            ColumnDef(name="age", data_type="integer", value_source="value", value_attribute="sum"),
         ]
-        types = [
-            'priv_trained', 'priv_ayush_trained', 'priv_allo_trained', 'priv_avg_diff', 'priv_gt80',
-            'pub_trained', 'pub_ayush_trained', 'pub_allo_trained', 'pub_avg_diff', 'pub_gt80',
-            'dep_trained', 'dep_pers_trained', 'dep_avg_diff', 'dep_gt80',
-            'flw_trained', 'flw_pers_trained', 'flw_avg_diff', 'flw_gt80',
-        ]
-        for c in types:
-            columns.append(ColumnDef(name=c, data_type="integer", value_source="value", value_attribute='sum',
-                                     match_keys=[KeyMatcher(index=3, value=c)]))    
 
         return columns        
