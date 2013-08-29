872de44bd40ed3e086c193574f4928af6ef210f2
Sravan Reddy <sravfeyn@gmail.com>
2013-08-29 11:12:47 +0530

    collapsed ctable into one

--- custom/apps/gsid/ctable_mappings.py
+++ custom/apps/gsid/ctable_mappings.py
@@ -21,70 +21,7 @@ class PatientSummaryMapping(CtableMappingFixture):
             ColumnDef(name="date", data_type="date", value_source="key", value_index=7),
             ColumnDef(name="diagnosis", data_type="string", value_source="key", value_index=8),
             ColumnDef(name="age", data_type="integer", value_source="value", value_attribute="sum"),       
-        ]
-
-        return columns
-
-class ResultsByDayMapping(CtableMappingFixture):
-    name = "results_by_day"
-    domains = ['gsid']
-    couch_view = 'gsid/patient_summary'
-    schedule_active = True
-
-    @property
-    def columns(self):
-        columns = [
-            ColumnDef(name="domain", data_type="string", value_source="key", value_index=0),
-            ColumnDef(name="disease_name", data_type="string", value_source="key", value_index=1),
-            ColumnDef(name="test_version", data_type="string", value_source="key", value_index=2),
-            ColumnDef(name="province", data_type="string", value_source="key", value_index=3),
-            ColumnDef(name="district", data_type="string", value_source="key", value_index=4),
-            ColumnDef(name="clinic", data_type="string", value_source="key", value_index=5),
-            ColumnDef(name="gender", data_type="string", value_source="key", value_index=6),
-            ColumnDef(name="date", data_type="date", value_source="key", value_index=7),
-        ]
-
-        return columns
-
-class TestLotsMapping(CtableMappingFixture):
-    name = "test_lots"
-    domains = ['gsid']
-    couch_view = 'gsid/patient_summary'
-    schedule_active = True
-
-    @property
-    def columns(self):
-        columns = [
-            ColumnDef(name="domain", data_type="string", value_source="key", value_index=0),
-            ColumnDef(name="disease_name", data_type="string", value_source="key", value_index=1),
-            ColumnDef(name="test_version", data_type="string", value_source="key", value_index=2),
-            ColumnDef(name="province", data_type="string", value_source="key", value_index=3),
-            ColumnDef(name="district", data_type="string", value_source="key", value_index=4),
-            ColumnDef(name="clinic", data_type="string", value_source="key", value_index=5),
-            ColumnDef(name="date", data_type="string", value_source="key", value_index=7),
             ColumnDef(name="lot_number", data_type="integer", value_source="key", value_index=9),
         ]
 
-        return columns
-
-class ResultsByAgeMapping(CtableMappingFixture):
-    name = "results_by_age"
-    domains = ['gsid']
-    couch_view = 'gsid/patient_summary'
-    schedule_active = True
-
-    @property
-    def columns(self):
-        columns = [
-            ColumnDef(name="domain", data_type="string", value_source="key", value_index=0),
-            ColumnDef(name="disease_name", data_type="string", value_source="key", value_index=1),
-            ColumnDef(name="test_version", data_type="string", value_source="key", value_index=2),
-            ColumnDef(name="province", data_type="string", value_source="key", value_index=3),
-            ColumnDef(name="district", data_type="string", value_source="key", value_index=4),
-            ColumnDef(name="clinic", data_type="string", value_source="key", value_index=5),
-            ColumnDef(name="gender", data_type="string", value_source="key", value_index=6),
-            ColumnDef(name="date", data_type="date", value_source="key", value_index=7),
-            ColumnDef(name="age", data_type="integer", value_source="value", value_attribute="sum"),
-        ]
-
-        return columns        
+        return columns      
