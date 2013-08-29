972ac9a8ac94502cca8ba7d5247c9789412d3ff7
Sravan Reddy <sravfeyn@gmail.com>
2013-08-29 11:00:40 +0530

    two reports done, two to go

--- custom/apps/gsid/reports/sql_reports.py
+++ custom/apps/gsid/reports/sql_reports.py
@@ -1,5 +1,6 @@
 from sqlagg.columns import *
 from sqlagg.base import AliasColumn
+from sqlagg.filters import *
 from sqlalchemy import func
 from corehq.apps.fixtures.models import FixtureDataItem, FixtureDataType
 from corehq.apps.reports.basic import Column
@@ -18,6 +19,7 @@ class GSIDSQLReport(SummingSqlTabularReport, CustomProjectReport, DatespanMixin)
     emailable = True
     slug = "my_basic_sql"
     section_name = "event demonstrations"
+    table_name = "gsid_patient_summary"
 
     @property
     def group_by(self):
@@ -57,61 +59,115 @@ class GSIDSQLPatientReport(GSIDSQLReport):
     name = "Patient Summary Report"
     slug = "patient_summary_sql"
     section_name = "patient summary"
-    table_name = "gsid_patient_summary"
-
+    
     @property
     def columns(self):
-        agg_fn = lambda x, y: str(x)+"-"+str(y)
-        sum_agg_fn = lambda x, y: x + y
+        age_fn = lambda x, y: str(x or "-")+"-"+str(y or "-")
+        sum_fn = lambda x, y: int(x or 0) + int(y or 0)
+        percent_agg_fn = lambda x, m, f: "%(x)s (%(p)s%%)" % {"x": x or 0, "p": (100*int(x or 0) / (sum_fn(m, f) or 1))}
+        total_percent_agg_fn = lambda x, y, m, f: "%(x)s (%(p)s%%)" % {"x": sum_fn(x, y), "p": (100*sum_fn(x, y) / (sum_fn(m, f) or 1))}
+
         patient_number_group = DataTablesColumnGroup("Tests")
         positive_group = DataTablesColumnGroup("Positive Tests")
         age_range_group = DataTablesColumnGroup("Age Range")
+
         return [
             DatabaseColumn("Clinic Name", SimpleColumn("clinic")),
-            DatabaseColumn("Number of Males ", CountColumn('gender', alias="male-total", filters=["gender = :male"]), header_group=patient_number_group),
-            DatabaseColumn("Number of Females ", CountColumn('gender', alias="female-total", filters=["gender = :female"]), header_group=patient_number_group),
-            DatabaseColumn("Number of Male Postives", CountColumn("diagnosis", alias="male-positive", filters=["gender = :male", "diagnosis = :positive"]), header_group=positive_group),
-            DatabaseColumn("Number of Female Postives", CountColumn("diagnosis", alias="female-positive", filters=["gender = :female", "diagnosis = :positive"]), header_group=positive_group),
-            #DatabaseColumn("Number of Male Postives", CountColumn("diagnosis", alias="male-positive", filters=["gender = :male", "diagnosis = :positive"])),
-            #DatabaseColumn("Number of Female Postives", CountColumn("diagnosis", alias="female-positive", filters=["gender = :female", "diagnosis = :positive"])),
-            #DatabaseColumn("num of", agg_fn, AliasColumn("male-positive"), AliasColumn("female-positive")),
-            DatabaseColumn("Total", AggregateColumn(sum_agg_fn, AliasColumn("male-total"), AliasColumn("female-total"))),
-            #CountColumn("gender", filters=['gender = :male']),
-            #DatabaseColumn("Total Male", CountColumn("gender", filters=['gender = :male'])),
-            #SumColumn("Total Female", filters=['sex = :female']),
-            #AggregateColumn("age", agg_fn, MaxColumn('age', filters=['gender = :male']), MinColumn('age', filters=['gender = :male'])),
-            #DatabaseColumn("Male range", AggregateColumn(agg_fn, MaxColumn('age', filters=['gender = :male']), MinColumn('age', filters=['gender = :male']))),            
+            
+            DatabaseColumn("Number of Males ", 
+                            CountColumn('gender', alias="male-total", filters=[EQ("gender", "male")]), 
+                            header_group=patient_number_group),
+            DatabaseColumn("Number of Females ", 
+                            CountColumn('gender', alias="female-total", filters=[EQ("gender", "female")]), 
+                            header_group=patient_number_group),
+            AggregateColumn("Total", sum_fn, 
+                            [AliasColumn("male-total"), AliasColumn("female-total")],
+                            header_group=patient_number_group),
+
+            AggregateColumn("Male +ve Percent", percent_agg_fn,
+                            [CountColumn("diagnosis", alias="male-positive", filters=[AND([EQ("gender", "male"), EQ("diagnosis", "positive")])]), 
+                             AliasColumn("male-total"), AliasColumn("female-total")],
+                            header_group=positive_group),
+            AggregateColumn("Female +ve Percent", percent_agg_fn,
+                            [CountColumn("diagnosis", alias="female-positive", filters=[AND([EQ("gender", "female"), EQ("diagnosis", "positive")])]), 
+                             AliasColumn("male-total"), AliasColumn("female-total")],
+                            header_group=positive_group),
+            AggregateColumn("Total +ve Percent", total_percent_agg_fn,
+                            [AliasColumn("female-positive"), 
+                             AliasColumn("male-positive"),
+                             AliasColumn("male-total"), AliasColumn("female-total")],
+                            header_group=positive_group),
+
+            AggregateColumn("Male age range", age_fn,
+                            [MinColumn("age", alias="male-min", filters=[EQ("gender", "male")]),
+                             MaxColumn("age", alias="male-max", filters=[EQ("gender", "male")])],
+                            header_group=age_range_group),
+            AggregateColumn("FeMale age range", age_fn,
+                            [MinColumn("age", alias="female-min", filters=[EQ("gender", "female")]),
+                             MaxColumn("age", alias="female-max", filters=[EQ("gender", "female")])],
+                            header_group=age_range_group),
+            AggregateColumn("All age range", age_fn,
+                            [MinColumn("age", alias="age-min"),
+                             MaxColumn("age", alias="age-max")],
+                            header_group=age_range_group),
         ]
 
 class GSIDSQLByDayReport(GSIDSQLReport):
     name = "Day Summary Report"
     slug = "day_summary_sql"
     section_name = "day summary"
-    table_name = "gsid_results_by_day"
-
+    
 class GSIDSQLTestLotsReport(GSIDSQLReport):
     name = "Test Lots Report"
     slug = "test_lots_sql"
     section_name = "test lots"
-    table_name = "gsid_test_lots"
-
+    
 class GSIDSQLByAgeReport(GSIDSQLReport):
     name = "Age Summary Report"
     slug = "age_summary_sql"
     section_name = "age summary"
-    table_name = "gsid_results_by_age"
-
+    
     @property
     def filter_values(self):
-        default_filter_values = super(GSIDSQLByAgeReport, self).filter_values
         age_filters = dict(zero=0,
                     ten=10,
+                    ten_plus=11,
                     twenty=20,
+                    twenty_plus=21,
                     fifty=50)
-        return default_filter_values.update(age_filters)
+        default_filter_values = super(GSIDSQLByAgeReport, self).filter_values
+        default_filter_values.update(age_filters)
+        return default_filter_values
 
     @property
     def columns(self):
-        return [
-            DatabaseColumn("Clinic Name", SimpleColumn("clinic")),
-        ]
+        percent_fn = lambda x, y: "%(x)s (%(p)s%%)" % {"x": int(x or 0), "p": 100*(x or 0) / (y or 1)}
+        
+        age_range_group = DataTablesColumnGroup("Positive Tests %positive")
+
+        def age_range_filter(gender, age_from, age_to):
+            return [AND([EQ("gender", gender), EQ("diagnosis", "positive"), BETWEEN("age", age_from, age_to)])]
+
+        def generate_columns(gender):
+            return [
+                AggregateColumn("0-10", percent_fn,
+                                [CountColumn("age", alias="zero_ten_" + gender, filters=age_range_filter(gender, "zero", "ten")),
+                                 CountColumn("age", alias=gender+"_total", filters=[EQ("gender", gender)])],
+                                header_group=age_range_group),
+                AggregateColumn("10-20", percent_fn, 
+                                [CountColumn("age", alias="ten_twenty_" + gender, filters=age_range_filter(gender, "ten_plus", "twenty")),
+                                 AliasColumn(gender+"_total")],
+                                header_group=age_range_group),
+                AggregateColumn("20-50", percent_fn,
+                                [CountColumn("age", alias="twenty_fifty_" + gender, filters=age_range_filter(gender, "twenty_plus", "fifty")),
+                                AliasColumn(gender+"_total")],
+                                header_group=age_range_group),
+                AggregateColumn("50+", percent_fn,
+                                [CountColumn("age", alias="fifty_" + gender, filters=[AND([EQ("gender", gender), EQ("diagnosis", "positive"), GT("age", "fifty")])]),
+                                AliasColumn(gender+"_total")],
+                                header_group=age_range_group),
+            ]
+
+        return [DatabaseColumn("Clinic Name", SimpleColumn("clinic"), header_group=age_range_group)
+               ] + generate_columns("male") + generate_columns("female")
+
