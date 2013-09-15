fbabb68110bdf98be3037e59b0697d4554b4e7c9
Sravan Reddy <sravfeyn@gmail.com>
2013-09-15 22:30:00 +0530

    better date formatting in chart axis

--- corehq/apps/reports/graph_models.py
+++ corehq/apps/reports/graph_models.py
@@ -1,7 +1,14 @@
 class Axis(object):
-    def __init__(self, label=None, format=None):
+    """
+    :param label: Name of the Axis
+    :param format: d3.js axis format
+    :param dateFormat: Modify values to JS Date objects and set d3.time.format 
+                       refer to https://github.com/mbostock/d3/wiki/Time-Formatting
+    """
+    def __init__(self, label=None, format=None, dateFormat=None):
         self.label = label
         self.format = format
+        self.dateFormat = dateFormat
 
 
 class Chart(object):
@@ -101,17 +108,6 @@ class LineChart(Chart):
         marginLeft: Left Margin in pixels
         marginRight: Right Margin in pixels
         marginBottom: Bottom Margin in pixels
-        showControls: True to show controls
-        showLegend: True to show legend
-        reduceXTicks: True to reduce the number of X ticks
-        rotateLabels: Degrees to rotate X-Axis labels e.g. -45
-        tooltips: True to show tooltips
-        stacked: True to make default view stacked, False for grouped
-        staggerLabels: True to stagger the X-Axis labels.
-        groupSpacing: Used to adjust amount of space between X-Axis groups. Value between 0 and 1.
-        forceY: Used to force values into the Y scale domain. Useful to ensure max / min scales. Must be list of numbers
-        translateLabelsX: Pixels to move X-Axis labels in X direction
-        translateLabelsY: Pixels to move X-Axis labels in Y direction
 
     """
     template_partial = 'reports/partials/graphs/line_chart.html'
@@ -126,17 +122,6 @@ class LineChart(Chart):
         self.marginRight = 20
         self.marginBottom = 50
         self.marginLeft = 100
-        self.showControls = True
-        self.showLegend = True
-        self.reduceXTicks = False
-        self.rotateLabels = 0
-        self.tooltips = True
-        self.stacked = False
-        self.translateLabelsX = 0
-        self.translateLabelsY = 0
-        self.staggerLabels = False
-        self.groupSpacing = 0.3
-        self.forceY = [0, 1]
 
     def add_dataset(self, key, values, color=None):
         """
@@ -148,26 +133,12 @@ class LineChart(Chart):
         if color:
             d['color'] = color
         self.data.append(d)
-        print self.data
 
     def config_dict(self):
-        if self.rotateLabels and not self.translateLabelsX:
-            self.translateLabelsX = -10
         return dict(margin={'top': self.marginTop,
                             'right': self.marginRight,
                             'bottom': self.marginBottom,
-                            'left': self.marginLeft},
-                    showControls=self.showControls,
-                    showLegend=self.showLegend,
-                    reduceXTicks=self.reduceXTicks,
-                    rotateLabels=self.rotateLabels,
-                    tooltips=self.tooltips,
-                    stacked=self.stacked,
-                    translateLabelsX=self.translateLabelsX,
-                    translateLabelsY=self.translateLabelsY,
-                    staggerLabels=self.staggerLabels,
-                    forceY=self.forceY,
-                    groupSpacing=self.groupSpacing)
+                            'left': self.marginLeft})
 
 
 class PieChart(Chart):
